354da81e6c6d1bfafb52931c8fdf2146f91f0332
Jinho227 <sreika227@gmail.com>
2022-09-28 10:28:41 +0900

    feat: 1546 solved

---
+++ BJ1546/BJ1546/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1546
+{
+    class MainApp
+    {
+        
+        static void Main(string[] args)
+        {
+            string numbers;
+            int number;
+            double sum = 0;
+            double Max = 0;
+            double[] score = new double[1000];
+            numbers = Console.ReadLine();
+            number = int.Parse(numbers);
+            string[] arr = Console.ReadLine().Split();
+
+            for (int i = 0; i < number; i++)
+            {
+                score[i] = double.Parse(arr[i]);
+                if (Max < score[i])
+                    Max = score[i];
+                sum += score[i];
+            }
+            Console.WriteLine(sum / Max * 100 / number);
+
+            return;
+        }
+    }
+}
