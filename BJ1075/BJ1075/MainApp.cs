7f1cd9691348bdf08de35ac630dabcc7f6e54bec
Jinho227 <sreika227@gmail.com>
2022-10-02 17:13:31 +0900

    feat: 1075 solved

---
+++ BJ1075/BJ1075/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1075
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int N, F;
+            N = int.Parse(Console.ReadLine());
+            F = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < 100; i++)
+            {
+                if ((N - (N % 100) + i) % F == 0)
+                {
+                    Console.WriteLine("{0:00}", i);
+                    break;
+                }
+            }
+            return;
+        }
+    }
+}
