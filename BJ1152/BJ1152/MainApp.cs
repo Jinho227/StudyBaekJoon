7de63a542564997dc0e424c165b643b13a484bbc
Jinho227 <sreika227@gmail.com>
2022-10-03 19:42:28 +0900

    feat: 1152 solved

---
+++ BJ1152/BJ1152/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1152
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string[] temp = Console.ReadLine().Split();
+
+            int number = temp.Length;
+            if (temp[0] == "")
+                number -= 1;
+            if (temp[(temp.Length - 1)] == "")
+                number -= 1;
+            Console.WriteLine(number);
+        }
+    }
+}
