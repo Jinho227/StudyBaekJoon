08a0c38cd7c50396ca32349b84f71dbb63fd424c
Jinho227 <sreika227@gmail.com>
2022-10-05 19:40:27 +0900

    feat: 1920 solved

---
+++ BJ1920/BJ1920/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1920
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder s = new StringBuilder();
+            int Nnumber = int.Parse(Console.ReadLine());
+            int[] temp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int Mnumber = int.Parse(Console.ReadLine());
+            int[] Mtemp = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            for (int i = 0; i < Mtemp.Length; i++)
+            {
+                if(temp.Contains(Mtemp[i]))
+                    s.AppendLine("1"); // wirteline은 속도가 느리다.
+                else
+                    s.AppendLine("0");
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
