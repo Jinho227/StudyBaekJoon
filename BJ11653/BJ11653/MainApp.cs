221623848a94783002d5e6d8a8ac60644eb1eab4
Jinho227 <sreika227@gmail.com>
2022-10-07 19:01:48 +0900

    feat: 11653 solved

---
+++ BJ11653/BJ11653/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ11653
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] output = new int[100];
+            int i = 0;
+            int j;
+            while(input != 1)
+            {
+                j = 2;
+                do
+                {
+                    if (input % j != 0)
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        output[i++] = j;
+                        input /= j;
+                        break;
+                    }
+                }
+                while (true);
+            }
+
+            for(int k = 0; k < i; k++)
+            {
+                Console.WriteLine(output[k]);
+            }
+        }
+    }
+}
