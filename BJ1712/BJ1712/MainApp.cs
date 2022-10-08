60bcac0ba067dc7fba8091eeeae0a99fa51b4c27
Jinho227 <sreika227@gmail.com>
2022-10-08 19:48:27 +0900

    feat: 1712 solved

---
+++ BJ1712/BJ1712/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1712
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string[] temp = new string[3];
+            int a, b, c;
+            long i = 0;
+            temp = Console.ReadLine().Split();
+            a = int.Parse(temp[0]);
+            b = int.Parse(temp[1]);
+            c = int.Parse(temp[2]);
+
+            if( b >= c)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                if(a % (c - b) == 0)
+                    i = (a / (c - b)) + 1;
+                else
+                    i = (a / (c - b)) + 1;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
