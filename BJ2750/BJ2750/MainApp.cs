01fcffc1c06dda95768b01d6195dc09aa6a33367
Jinho227 <sreika227@gmail.com>
2022-10-09 14:23:36 +0900

    feat: 2750 solved

---
+++ BJ2750/BJ2750/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ2750
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int number;
+
+            number = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < number; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+            list.Sort();
+            foreach(int output in list)
+            {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
