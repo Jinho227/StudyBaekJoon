4fe95142c7d835c9fdd9fcb6fc7afa2c7bd6a872
Jinho227 <sreika227@gmail.com>
2022-10-10 20:32:36 +0900

    feat: 2525 solved

---
+++ BJ2525/BJ2525/MainApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ2525
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split();
+            int h, m;
+            h = int.Parse(a[0]);
+            m = int.Parse(a[1]);
+
+            int p = int.Parse(Console.ReadLine());
+
+            if(m + p < 60)
+            {
+                Console.WriteLine(h + " " + (m+p));
+            }
+            else
+            {
+                if ((h + (p / 60) + ((m + (p % 60)) / 60)) >= 24)
+                {
+                    if(m+(p%60) >= 60)
+                        Console.WriteLine((h + (p / 60) - 24 + ((m + (p % 60)) / 60)) + " " + ((m + (p % 60)) % 60));
+                    else
+                    {
+                        Console.WriteLine((h + (p / 60) - 24) + " " + (m + (p % 60)));
+                    }
+                }
+                   
+                else
+                {
+                    if (m + (p % 60) >= 60)
+                        Console.WriteLine((h + (p / 60) + ((m + (p % 60)) / 60)) + " " + ((m + (p % 60)) % 60));
+                    else
+                    {
+                        Console.WriteLine((h + (p / 60)) + " " + (m + (p % 60)));
+                    }
+                }
+            }
+        }
+    }
+}
