c13be4ccc7dc3e79852895cb83d62ae2df0b052a
Jinho227 <sreika227@gmail.com>
2022-10-11 19:11:25 +0900

    feat: 10818 solved

---
+++ BJ10818/BJ10818/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ10818
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string[] a = Console.ReadLine().Split();
+            int min = 1000000;
+            int max = -1000000;
+            foreach(string s in a)
+            {
+                if (min > int.Parse(s))
+                    min = int.Parse(s);
+                if (max < int.Parse(s))
+                    max = int.Parse(s);
+            }
+            Console.WriteLine(min + " " + max);
+        }
+    }
+}
