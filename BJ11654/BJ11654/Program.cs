b7702c18af39161bf80242b4a08fd5c0518fc72c
Jinho227 <sreika227@gmail.com>
2022-10-12 20:50:14 +0900

    feat: 11654 solved

---
+++ BJ11654/BJ11654/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ11654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            byte[] b = Encoding.Default.GetBytes(a);
+            Console.WriteLine(b[0]);
+        }
+    }
+}
