d0a14177ce7fba9b7ec622b264c0e521f716ffdf
Jinho227 <sreika227@gmail.com>
2022-10-18 21:53:38 +0900

    feat: 2798 solved

---
+++ BJ2798/BJ2798/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ2798
+{
+    class MainApp
+    {
+        static int BlackJack(int[] card, int M)
+        {
+            int sum = 0;
+            for(int i = 0; i < card.Length; i++)
+            {
+                for (int j = i + 1; j < card.Length; j++)
+                {
+
+                    for (int k = j + 1; k < card.Length; k++)
+                    {
+                        int temp = card[i] + card[j] + card[k];
+                        if (temp <= M)
+                            if (temp > sum)
+                                sum = temp;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int N = Convert.ToInt32(s[0]);
+            int M = Convert.ToInt32(s[1]);
+            int[] card = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Console.WriteLine(BlackJack(card, M));
+
+        }
+    }
+}
