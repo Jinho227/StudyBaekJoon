3e3f423b4b25b84f5ca815da00df5e00e205f79d
Jinho227 <sreika227@gmail.com>
2022-10-19 21:14:53 +0900

    feat: 10872 solved

---
+++ BJ10872/BJ10872/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ10872
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            int output = 1;
+            if (a == 0)
+                Console.WriteLine(1);
+            else
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    output *= i;
+                }
+                Console.WriteLine(output);
+            }
+            
+        }
+    }
+}
