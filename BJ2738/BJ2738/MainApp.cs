04954471a787566f90538bf7fdbb0ee11520662a
Jinho227 <sreika227@gmail.com>
2022-11-02 19:42:59 +0900

    feat: 2738 solved

---
+++ BJ2738/BJ2738/MainApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ2738
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            string[] num = Console.ReadLine().Split();
+            n = int.Parse(num[0]);
+            m = int.Parse(num[1]);
+
+            int[,] input = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                string[] temp = Console.ReadLine().Split();
+                for (int j = 0; j < m; j++)
+                {
+                    input[i, j] = int.Parse(temp[j]);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                string[] temp = Console.ReadLine().Split();
+                for (int j = 0; j < m; j++)
+                {
+                    input[i, j] += int.Parse(temp[j]);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(input[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            return;
+        }
+    }
+}
