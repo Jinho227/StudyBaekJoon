57fde67c17b586c3d66721b297775b8ad4fbb48c
Jinho227 <sreika227@gmail.com>
2023-01-11 20:58:07 +0900

    feat: 1110 solved

---
+++ BJ1110/BJ1110/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BJ1110
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int temp = Int32.Parse(Console.ReadLine());
+            int N = temp;
+            int N10;
+            int N1;
+            int i = 0;
+            while (true)
+            {
+                N10 = N / 10;
+                N1 = N % 10;
+
+                N  = (N1 * 10) + ((N10 + N1) % 10);
+                i++;
+                if (N == temp)
+                    break;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
