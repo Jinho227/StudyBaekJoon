f968bf88b6c21f3275ee416af635f08ec86cdf84
Jinho227 <sreika227@gmail.com>
2023-01-12 23:08:58 +0900

    feat: 1094 solved

---
+++ BJ1094/BJ1094/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BJ1094
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            int x = 64;
+            int i = 0;
+            int sum = 0;
+            if (N == 64)
+            {
+                i++;
+            }
+            else
+            {
+                while (true)
+                {
+                    if (N < sum)
+                        sum -= x;
+                    else if (N > sum)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    x /= 2;
+                    sum += x;
+                }
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
