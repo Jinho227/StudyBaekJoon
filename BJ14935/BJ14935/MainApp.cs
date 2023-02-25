4be96e227a60f1b5450d7a07633feb90b4243d24
Jinho227 <sreika227@gmail.com>
2023-02-25 22:19:50 +0900

    feat: 14935 solved

---
+++ BJ14935/BJ14935/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ14935
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+
+            Console.WriteLine("FA");
+        }
+    }
+}
