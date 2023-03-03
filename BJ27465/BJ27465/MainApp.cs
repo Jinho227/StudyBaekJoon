b32504a619815ed1b1ba0ffed56ee5b46d6f2c37
Jinho227 <sreika227@gmail.com>
2023-03-03 23:52:08 +0900

    feat: 27465 solved

---
+++ BJ27465/BJ27465/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ27465
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            if (N <= 3)
+                Console.WriteLine(4);
+            else
+            {
+                if (N % 2 == 0)
+                    Console.WriteLine(N);
+                else if (N == 1000000000)
+                    Console.WriteLine(N);
+                else
+                    Console.WriteLine(N + 1);
+            }
+            
+
+        }
+    }
+}
