637a66d8bf2748fcaf65527d95be0fa83c0c03dd
Jinho227 <sreika227@gmail.com>
2023-03-05 22:27:09 +0900

    feat: 11399 solved

---
+++ BJ11399/BJ11399/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ11399
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            int[] t = new int[n];
+            string[] s = Console.ReadLine().Split();
+            int sum = 0;
+            int total = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                t[i] = Int32.Parse(s[i]);
+            }
+
+            Array.Sort(t);
+
+            for(int i = 0; i < n; i++)
+            {
+                sum += t[i];
+                total += sum;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
