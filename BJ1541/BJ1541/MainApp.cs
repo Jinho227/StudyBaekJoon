ad75c7b9282c370b1653bf8b771ef3d471696f3e
Jinho227 <sreika227@gmail.com>
2023-03-07 22:53:52 +0900

    feat: 1541 solved

---
+++ BJ1541/BJ1541/MainApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace BJ1541
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(-)|(\+)");
+            string[] s = regex.Split(Console.ReadLine());
+            bool m = false;
+            int sum = 0;
+            foreach(string result in s)
+            {
+                if(result == "+")
+                {
+
+                }
+                else if(result == "-")
+                {
+                    m = true;
+                }
+                else
+                {
+                    if(m == true)
+                    {
+                        sum -= Int32.Parse(result);
+                    }
+                    else
+                    {
+                        sum += Int32.Parse(result);
+                    }
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
