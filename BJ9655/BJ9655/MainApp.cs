45af5c7b04e8aaff001b163c20b307ab79716d30
Jinho227 <sreika227@gmail.com>
2023-03-11 22:41:19 +0900

    feat: 9655 solved

---
+++ BJ9655/BJ9655/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ9655
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            if(n % 2 == 0)
+            {
+                Console.WriteLine("CY");
+            }
+            else
+            {
+                Console.WriteLine("SK");
+            }
+        }
+    }
+}
