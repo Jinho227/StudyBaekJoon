6a2c2069f246584f8ac85cee51989379fdade1e7
Jinho227 <sreika227@gmail.com>
2023-03-12 23:54:07 +0900

    feat: 10817 solved

---
+++ BJ10817/BJ10817/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ10817
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int[] n = new int[3];
+            
+            for(int i = 0; i < 3; i++)
+            {
+                n[i] = Int32.Parse(s[i]);
+            }
+            Array.Sort(n);
+            Console.WriteLine(n[1]);
+        }
+    }
+}
