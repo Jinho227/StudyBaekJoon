3f975885df097bf343bbaacd8b3b2d6abbf6e1ee
Jinho227 <sreika227@gmail.com>
2023-03-13 22:09:01 +0900

    feat: 2748 solved

---
+++ BJ2748/BJ2748/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ2748
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ulong f1 = 0;
+            ulong f2 = 1;
+            ulong f3;
+            int n = Int32.Parse(Console.ReadLine());
+            ulong a = 0;
+
+            for(int i = 0; i <= n; i++)
+            {
+                if (i == 0)
+                {
+                    a = 0;
+                }
+                else if(i == 1)
+                {
+                    a = 1;
+                }
+                else
+                {
+                    f3 = f1 + f2;
+                    a = f3;
+                    f1 = f2;
+                    f2 = f3;
+                }
+            }
+            Console.WriteLine(a);
+        }
+
+
+    }
+}
