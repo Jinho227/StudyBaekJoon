7fff2ee6eb2389a65c4f13c62fd4d191211cba0f
Jinho227 <sreika227@gmail.com>
2023-03-14 22:42:25 +0900

    feat: 10988 solved

---
+++ BJ10988/BJ10988/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ10988
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] c = new char[s.Length];
+            int a = 1;
+            int i = 0;
+            foreach(char temp in s)
+            {
+                c[i] = temp;
+                i++;
+            }
+            for(i = 0; i < s.Length; i++)
+            {
+                if(c[i] != c[s.Length - (i + 1)])
+                {
+                    a = 0;
+                    break;
+                }
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
