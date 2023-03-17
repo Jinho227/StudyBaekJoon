14357f0a94c522f4744f0d40a58e677ba9d57d23
Jinho227 <sreika227@gmail.com>
2023-03-17 16:31:54 +0900

    feat: 1402 solved

---
+++ BJ1402/BJ1402/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1402
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            string[] s = new string[n];
+
+            /* 모든 것이 yes이기 때문에 */
+            for(int i = 0; i < n; i++)
+            {
+                s[i] = Console.ReadLine();
+            }
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("yes");
+            }
+
+        }
+    }
+}
