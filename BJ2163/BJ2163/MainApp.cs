85f57b7ef975ea423bf89bda786332955f61c28b
Jinho227 <sreika227@gmail.com>
2023-03-18 23:31:24 +0900

    feat: 2163 solved

---
+++ BJ2163/BJ2163/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ2163
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            int n = Int32.Parse(s[0]);
+            int m = Int32.Parse(s[1]);
+
+            Console.WriteLine(m * n - 1);
+        }
+    }
+}
